22e229b3d4acce953157043f9ca8c086dc344dc3
saikiran-bpr <saikiranbompelliwar0592@gmail.com>
2026-04-14 00:27:53 +0530

    screen.c

--- libs/screen.h
+++ libs/screen.h
@@ -3,8 +3,9 @@
 
 void scr_clear(void);
 void scr_move(int row, int col);
-void scr_putchar(char c);
-void scr_puts(const char *s);
-void scr_render_int(int n);
+void scr_putchar(int row, int col, char ch);
+void scr_puts(int row, int col, const char *s);
+void scr_render_int(int row, int col, int n);
+void scr_draw_border(int top, int left, int height, int width);
 
 #endif /* MY_SCREEN_H */
